84af99d120fb3993295fa4fa601bb6794fd0ea77
Spiderhex <mikolaj.majusiak@gmail.com>
2026-04-25 17:16:39 +0200

    Engine optimization

--- engine.py
+++ engine.py
@@ -108,7 +108,7 @@ def evaluate_board(board):
 
     return score
 
-def evaluate_move(board, move):
+def score_move(board, move):
     if board.is_capture(move):
         victim = board.piece_type_at(move.to_square)
         attacker = board.piece_type_at(move.from_square)
@@ -119,16 +119,52 @@ def evaluate_move(board, move):
         return PIECE_VALUES[victim] - PIECE_VALUES[attacker]
     return 0
 
-def min_max_move_with_alpha_beta(board, alpha=MIN, beta=MAX, depth=3):
+def evaluate_move(board, move):
+    attacker_type = board.piece_type_at(move.from_square)
+    if attacker_type is None:
+        return 0
+
+    piece = board.piece_at(move.from_square)
+    color = piece.color
+    table = PIECE_SQUARE_TABLES[attacker_type]
+
+    if color == chess.WHITE:
+        before = table[move.from_square]
+        after = table[move.to_square]
+        sign = 1
+    else:
+        before = table[chess.square_mirror(move.from_square)]
+        after = table[chess.square_mirror(move.to_square)]
+        sign = -1
+
+    delta = after - before
+
+    if board.is_capture(move):
+        victim_type = board.piece_type_at(move.to_square)
+
+        if victim_type is None:
+            victim_value = PIECE_VALUES[chess.PAWN]  # en passant
+        else:
+            victim_value = PIECE_VALUES[victim_type]
+
+        delta += victim_value
+
+    return sign * delta
+
+def min_max_move_with_alpha_beta(board, evaluation, alpha=MIN, beta=MAX, depth=3):
     if depth == 0 or board.is_game_over():
-        return evaluate_board(board)
+        return evaluation
+
     moves = list(board.legal_moves)
-    moves.sort(key=lambda m: evaluate_move(board, m), reverse=True)
+    moves.sort(key=lambda m: score_move(board, m), reverse=True)
+
     if board.turn:
         best_score = MIN
+
         for move in moves:
+            delta = evaluate_move(board, move)
             board.push(move)
-            best_score = max(best_score, min_max_move_with_alpha_beta(board, alpha, beta, depth-1))
+            best_score = max(best_score, min_max_move_with_alpha_beta(board, evaluation + delta, alpha, beta, depth-1))
             alpha = max(best_score, alpha)
             board.pop()
             if beta <= alpha:
@@ -138,8 +174,9 @@ def min_max_move_with_alpha_beta(board, alpha=MIN, beta=MAX, depth=3):
     else:
         best_score = MAX
         for move in moves:
+            delta = evaluate_move(board, move)
             board.push(move)
-            best_score = min(best_score, min_max_move_with_alpha_beta(board, alpha, beta, depth-1))
+            best_score = min(best_score, min_max_move_with_alpha_beta(board, evaluation + delta, alpha, beta, depth-1))
             beta = min(best_score, beta)
             board.pop()
             if beta <= alpha:
@@ -155,9 +192,12 @@ def find_best_move(board):
     best_move = []
     moves = list(board.legal_moves)
     moves.sort(key=lambda m: board.is_capture(m), reverse=True)
+
+    current_eval = evaluate_board(board)
     for move in moves:
+        delta = evaluate_move(board, move)
         board.push(move)
-        score = min_max_move_with_alpha_beta(board)
+        score = min_max_move_with_alpha_beta(board, current_eval + delta)
         board.pop()
         if board.turn:
             if score > best_score:
@@ -173,4 +213,7 @@ def find_best_move(board):
                 best_move.append(move)
 
     return random.choice(best_move)
+
+BOARD = chess.Board()
+BOARD.push(chess.Move.from_uci("e2e4"))
 
